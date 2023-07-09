6cd9bfb0a03ca17a01a7bb43978f508b6ee0b495
Thiagojm <tjm.plastica@gmail.com>
2023-07-09 14:12:25 -0300

    multi_page

--- pages/About.py
+++ pages/About.py
@@ -1,4 +1,6 @@
-# Streamlit RNG Data Visualization App
+import streamlit as st
+
+st.markdown("""# Streamlit RNG Data Visualization App
 
 An application that takes TRNG data from an AWS S3 Bucket and analyzes it into a graph and a dataframe.
 
@@ -6,7 +8,4 @@ An application that takes TRNG data from an AWS S3 Bucket and analyzes it into a
 
 - Login functionality with username and password verification;
 - The user can select which file he wants to work on in the side menu;
-- After clicking on "Show Graph", a graph will be displayed with the data and the standard deviation and, below, the dataframe used.
-
-## Online
-https://rng-charts.streamlit.app/
+- After clicking on "Show Graph", a graph will be displayed with the data and the standard deviation and, below, the dataframe used.""")
